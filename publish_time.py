ededff3ab094c8a7e65b4872e919766d377069a7
Klaas Hofman <klaas@mind.be>
2018-04-29 14:57:57 +0200

    Replaced broker host name by 'pi2.local', fixed typo in subscription data

--- publish_time.py
+++ publish_time.py
@@ -8,7 +8,7 @@ import paho.mqtt.client as mqtt
 import time
 
 client = mqtt.Client()
-client.connect("iot.eclipse.org", 1883, 60)
+client.connect("pi2.local", 1883, 60)
 
 while True:
     mypayload = time.strftime("%I:%M:%S")
--- subscribe_time.py
+++ subscribe_time.py
@@ -5,11 +5,9 @@ def on_message(client, userdata, message):
     print message.payload
 
 client = mqtt.Client()
-client.connect("iot.eclipse.org", 1883, 60)
-client.subscribe("mydata")
+client.connect("pi2.local", 1883, 60)
+client.subscribe("mydataklaas")
 
 client.on_message = on_message
-
 print("waiting....")
 client.loop_forever()
-
